57a936700a3d9b71340550032181aea19f79baeb
Noquiy <nominos09@gmail.com>
2024-01-23 18:40:19 +0100

    icons for different skies

--- client/src/pages/Departures.tsx
+++ client/src/pages/Departures.tsx
@@ -1,8 +1,8 @@
 
 import * as React from "react";
 import { Button } from "../components/Button";
-import { PlaneTakeoff } from 'lucide-react'
-import Letter  from './Letter';
+import { PlaneTakeoff, Cloudy, Sun, Thermometer, CloudRainWind, Cloud } from 'lucide-react'
+import Letter from './Letter';
 
 export function Departures() {
     const [departuresData, setdeparturesData] = React.useState([])
@@ -35,7 +35,7 @@ export function Departures() {
             setTimer(prevTimer => prevTimer - 1);
         }, 1000);
 
-        
+
         if (timer === 0) {
             setTimer(300);
             fetchArrivals();
@@ -49,32 +49,53 @@ export function Departures() {
         const hours = date.getHours().toString().padStart(2, '0');
         const minutes = date.getMinutes().toString().padStart(2, '0');
         return `${hours}:${minutes}`;
-      };
+    };
+
+    const hourNow = new Date().getHours()
+    const minuteNow = new Date().getMinutes()
 
     return (
         <div className="bg-white w-screen h-screen font-sans">
-            <div className="flex flex-row">
-                <PlaneTakeoff className="text-zinc-800 h-8 w-8 mt-1 mr-2"/>
-                <h1 className="text-zinc-800 font-bold text-4xl">Departures</h1>
+            <div className="flex flex-row justify-center items-center -mt-6">
+                <p className="text-zinc-800 font-bold text-6xl px-20 items-start justify-start">
+                    {minuteNow < 10 ? hourNow + ":0" + minuteNow : hourNow + ":" + minuteNow}
+                </p>
+                <div className="flex flex-row px-20">
+                    <PlaneTakeoff className="text-zinc-800 h-14 w-14 mr-2" />
+                    <h1 className="text-zinc-800 font-bold text-6xl">DEPARTURES</h1>
+                </div>
+                <img src="https://www.airport-suppliers.com/wp-content/uploads/2016/02/warsaw-chopin-airport-CMYK.jpg" alt="Warsaw Chopin Airport" className="h-32 px-20" />
             </div>
             <div className='inline-flex'>
-                <table className="table-auto text-zinc-800 text-2xl w-screen">
+                <table className="table-auto text-zinc-800 text-xl w-screen">
                     <thead>
-                        <tr>
+                        <tr className="bg-cyan-400">
                             <th className="px-4 py-2">Airline</th>
-                            <th className="px-4 py-2">Flying to</th>
-                            <th className="px-4 py-2">Flight number</th>
-                            <th className="px-4 py-2">Estimated</th>
+                            <th className="px-4 py-2">Scheduled</th>
+                            <th className="px-4 py-2 w-1/6">Destination</th>
+                            <th className="px-4 py-2">Flight</th>
+                            <th className="px-4 py-2 w-1/6">Destination weather</th>
                             <th className="px-4 py-2">Status</th>
                         </tr>
                     </thead>
                     <tbody>
                         {departuresData.map((departure: any) => (
                             <tr key={departure.flight}>
-                                <td className="border px-4 py-4"><div className="flex flex-row gap-x-10 justify-center items-center">{departure.airline_logo ? <img className="" src={departure.airline_logo} alt=""/> :departure.airline}</div></td>
-                                <td className="border px-4 py-2">{departure.destination_city}</td>
-                                <td className="border px-4 py-2">{departure.flight_number}</td>
-                                <td className="border px-4 py-2">{formatTime(departure.expected_departure_time)}</td>
+                                <td className="border px-4 py-4"><div className="flex flex-row gap-x-10 justify-center items-center">{departure.airline_logo ? <img className="" src={departure.airline_logo} alt="" /> : departure.airline}</div></td>
+                                <td className="border px-4 py-2"><div className="flex flex-row justify-center items-center">{formatTime(departure.expected_departure_time)}</div></td>
+                                <td className="border px-4 py-2"><div className="flex flex-row justify-center items-center">{departure.destination_city}</div></td>
+                                <td className="border px-4 py-2"><div className="flex flex-row justify-center items-center">{departure.flight_number}</div></td>
+                                <td className="border px-4 py-2">
+                                    <div className="flex flex-row items-center justify-center">
+                                        <Thermometer /><span>{departure.destination_airport_temperature + "°C"}</span>
+                                        <span className="ml-5 mt-1">
+                                        {departure.destination_airport_sky_condition === 'Cloudy' ? <Cloud/> : 
+                                        departure.destination_airport_sky_condition === 'Rain' ? <CloudRainWind/> : 
+                                        departure.destination_airport_sky_condition === 'Overcast' ? <Cloudy/> :
+                                        departure.destination_airport_sky_condition === 'Clear' ? <Sun/> : null
+                                        }</span>
+                                    </div>
+                                </td>
                                 <td className="border px-4 py-2">{departure.status}</td>
                             </tr>
                         ))}

002caf3fa03d6364e4acb5efbd80f42096b1936e
Noquiy <nominos09@gmail.com>
2024-01-23 16:11:42 +0100

    Fixes to departures.py

--- server/app/departure.py
+++ server/app/departure.py
@@ -10,22 +10,19 @@ class Departure:
     def getScheduledDeparturesAtAirport(self) -> list:
         warsaw_airport = self.fr.get_airport(code = self.origin_airport_icao_code, details=True)
         departure_objects = []
+      
         for i in range(self.entries_limit):
             single_departure_object = warsaw_airport.departures['data'][i]['flight']
             departure_object_list = {
                 'flight_number': single_departure_object['identification']['number']['default'],
-                'airline': single_departure_object['airline']['name'],
-                'status': single_departure_object['status']['text'],
-                'destination_city': single_departure_object['airport']['destination']['position']['region']['city'],
+                'airline': single_departure_object['owner']['name'] if single_departure_object['airline'] is not None else 'Unknown',
+                'airline_logo': single_departure_object['owner']['logo'] if single_departure_object['airline'] is not None else 'Unknown',
+                'status': single_departure_object['status']['text'] if single_departure_object['status'] is not None else 'Unknown',
+                'destination_city': single_departure_object['airport']['destination']['position']['region']['city'] if single_departure_object['airport'] is not None else 'Unknown',
                 'expected_departure_time': single_departure_object['time']['scheduled']['arrival']
             }
             
             departure_objects.append(departure_object_list)
             
         return departure_objects
-    
-    """
-    def __getDestinationAirportWeatherDetails(self, destination_airport_icao_code:str) -> dict:
-        add weather forecast returing temperature and weather conditions(cloudy, sunny, rainy, etc.)
-    """
     
